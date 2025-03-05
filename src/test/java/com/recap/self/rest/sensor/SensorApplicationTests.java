255a7f1f9f84c9c4f1d56892b65f9326ae20555e
Maksim Zarutski <zarutski93maksim@gmail.com>
2025-03-05 18:23:01 +0300

    initial commit

---
+++ src/test/java/com/recap/self/rest/sensor/SensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recap.self.rest.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
